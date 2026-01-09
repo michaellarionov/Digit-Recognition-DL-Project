ebc39f7e7f33fc94022efbff109e97acefca9d44
Michael Larionov <michaellarionov@Michaels-MacBook-Air-92.local>
2026-01-09 10:52:41 -0500

    Forward Method

--- digitRecognition.py
+++ digitRecognition.py
@@ -28,3 +28,12 @@ class CNN(nn.module):
         self.conv2_drop=nn.Dropout2d()
         self.fc1=nn.Linear(320,50)
         self.fc2=nn.Linear(50,10)
+
+    def forward(self,x):
+        x=F.relu(F.max_pool2d(self.conv1(x),2))
+        x=F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)),2))
+        x=x.view(-1,320)
+        x=F.relu(self.fc1(x))
+        x=F.dropout(x,training=self.training)
+        x=self.fc2(x)
+        return F.log_softmax(x)
