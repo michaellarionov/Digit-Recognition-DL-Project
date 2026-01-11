fc8f79e13e2b27369a1f21244428fb4573bb65b3
Michael Larionov <michaellarionov@Michaels-MacBook-Air-92.local>
2026-01-11 13:20:09 -0500

    LeNet Transformation

--- digitRecognition.py
+++ digitRecognition.py
@@ -5,6 +5,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+transform = transforms.Compose([
+    transforms.Pad(2),      # 28×28 → 32×32
+    transforms.ToTensor()
+])
+
 trainingData=datasets.MNIST(root="data"
                             , train=True
                             , download=True
@@ -20,20 +25,24 @@ loaders = {
     'test': DataLoader(testData, batch_size=100, shuffle=True, num_workers=1),
 }
 # Define the CNN architecture
-class CNN(nn.module):
+class LeNet5(nn.Module):
     def __init__(self):
-        super(CNN,self).__init__()
-        self.conv1=nn.Conv2d(1,10,kernel_size=5)
-        self.conv2=nn.Conv2d(10,20,kernel_size=5)
-        self.conv2_drop=nn.Dropout2d()
-        self.fc1=nn.Linear(320,50)
-        self.fc2=nn.Linear(50,10)
+        super(LeNet5, self).__init__()
 
-    def forward(self,x):
-        x=F.relu(F.max_pool2d(self.conv1(x),2))
-        x=F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)),2))
-        x=x.view(-1,320)
-        x=F.relu(self.fc1(x))
-        x=F.dropout(x,training=self.training)
-        x=self.fc2(x)
-        return F.log_softmax(x)
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)      
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)     
+        self.conv3 = nn.Conv2d(16, 120, kernel_size=5)   
+
+        self.fc1 = nn.Linear(120, 84)                    
+        self.fc2 = nn.Linear(84, 10)                     
+
+    def forward(self, x):
+        x = F.avg_pool2d(torch.tanh(self.conv1(x)), 2)
+        x = F.avg_pool2d(torch.tanh(self.conv2(x)), 2)
+        x = torch.tanh(self.conv3(x))
+
+        x = x.view(x.size(0), -1)
+        x = torch.tanh(self.fc1(x))
+        x = self.fc2(x)
+
+        return F.log_softmax(x, dim=1)
